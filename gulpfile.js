b963b25a3c0ddb09b1693617690c23661d4d58d5
Leroy Bek <leroy@yorell.nl>
2015-03-09 23:32:51 +0100

    Add Less and JS support

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    plumber = require('gulp-plumber'),
     bowerFiles = require('main-bower-files'),
+    del = require('del'),
     concat = require('gulp-concat'),
     filter = require('gulp-filter'),
-    browserSync = require('browser-sync');
+    less = require('gulp-less'),
+    autoprefix = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload;
 
 var filterByExtension = function(extension){
     return filter(function(file){
@@ -18,33 +24,60 @@ gulp.task('bower', function() {
     gulp.src(bowerFiles())
         .pipe(jsFilter)
         .pipe(concat('plugins.js'))
-        .pipe(gulp.dest('./test/js'))
+        .pipe(gulp.dest('./test/assets/js'))
         .pipe(jsFilter.restore())
         .pipe(cssFilter)
         .pipe(concat('plugins.css'))
-        .pipe(gulp.dest('./test/css'));
+        .pipe(gulp.dest('./test/assets/css'));
 });
 
 gulp.task('jquery', function() {
     var file = bowerFiles({includeSelf: true, paths: { bowerJson: 'bower_components/jquery/bower.json'}});
 
     gulp.src(file)
-        .pipe(gulp.dest('./test/js/vendor'));
+        .pipe(gulp.dest('./test/assets/js/vendor'));
 });
 gulp.task('modernizr', function() {
     var file = bowerFiles({includeSelf: true, paths: { bowerJson: 'bower_components/components-modernizr/bower.json'}});
 
     gulp.src(file)
-        .pipe(gulp.dest('./test/js/vendor'));
+        .pipe(gulp.dest('./test/assets/js/vendor'));
 });
 gulp.task('normalize', function() {
     var file = bowerFiles({includeSelf: true, paths: { bowerJson: 'bower_components/normalize-css/bower.json'}});
 
     gulp.src(file)
-        .pipe(gulp.dest('./test/css'));
+        .pipe(gulp.dest('./test/assets/css'));
 });
 
+// Less
+gulp.task('less', function() {
+    gulp.src('src/less/main.less')
+        .pipe(plumber({
+            errorHandler: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(less())
+        .pipe(autoprefix('last 2 version', 'ie 9'))
+        .pipe(gulp.dest('test/assets/css'))
+        .pipe(reload({stream:true}));
+});
 
+// Javascript
+gulp.task('javascript', function() {
+    gulp.src('src/js/**/*.js')
+        .pipe(plumber({
+            errorHandler: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('test/assets/js'))
+        .pipe(reload({stream:true}))
+});
 
 gulp.task('browser-sync', function () {
     browserSync.init({
@@ -58,12 +91,23 @@ gulp.task('browser-sync', function () {
 
 
 
-
+// Clean
+gulp.task('clean:test', function(cb) {
+    del(['test'], cb)
+});
 
 gulp.task('default', function() {
-    gulp.run('bower', 'jquery', 'modernizr', 'normalize', 'browser-sync');
+    gulp.run('clean:test', 'bower', 'jquery', 'modernizr', 'normalize', 'less', 'javascript', 'browser-sync', 'watch');
 });
 
 gulp.task('build', function() {
 
+});
+
+gulp.task('watch', function() {
+    // Watch .less files
+    gulp.watch('src/less/**/*.less', ['less']);
+
+    // Watch .js files
+    gulp.watch('src/js/**/*.js', ['js']);
 });
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+(function ($) {
+
+    'use strict';
+
+    $(function () {
+        // Code goes here
+    });
+
+})(jQuery);
